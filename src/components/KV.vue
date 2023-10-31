41bb24f384df4850dd9677652fe114e5ad280e52
TITANSOFT\carol.yan99 <carol.yan@titansoft.com.sg>
2023-11-01 00:05:21 +0800

    init

---
+++ src/components/KV.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+defineProps<{ imgUrl:string}>()
+
+</script>
+
+<template>
+  <div class="KV">
+    <img id="banner" :src="imgUrl" crossOrigin>
+    <canvas id="source" width="1920" height="600"></canvas>
+  </div>
+</template>
+
+<style scoped>
+.KV{
+  width: 100%;
+  aspect-ratio: 16/5;
+}
+#banner{
+  width: 100%;
+}
+canvas{
+  display:none;
+}
+</style>
